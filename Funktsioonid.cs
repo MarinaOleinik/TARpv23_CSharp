286efdd4127e4718918e9f8a6a3120b2a92b133b
marinaoleinik <marina.oleinik@tthk.ee>
2024-08-26 16:15:16 +0300

    Add project files.

---
+++ Funktsioonid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_CSharp
+{
+    public class Funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}",nimi);
+        }
+
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+        // Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile.
+        public static double Arvuta_2(string exp)
+        {
+            double vastus=Convert.ToDouble(new System.Data.DataTable().Compute(exp,null));
+            return vastus;
+        }
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TARpv23_CSharp.Properties;
+
+namespace TARpv23_CSharp
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //I.osa Andmetüübid, Alamfunktsioonid,If
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello, {0}",nimetus);
+            Funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimene arv: ");
+            int a =int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus=Funktsioonid.Liitmine(a,b);
+            Console.WriteLine(vastus);
+            double arv = 5.123456;
+            vastus=Funktsioonid.Liitmine(a,(int)arv);
+            //Console.WriteLine(vastus);
+            char taht = 'A';
+            if (vastus==0) // &&-and, ||-or, !-not
+            {
+                Console.WriteLine(taht);
+            }
+            else 
+            { 
+                Console.WriteLine(vastus);
+            }
+            var lause= Console.ReadLine();
+            arv=Funktsioonid.Arvuta(lause);
+            Console.WriteLine(arv);
+        }
+
+    }
+}
