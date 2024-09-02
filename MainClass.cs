89ed46e3b42c1485585a1c92a22eb9bdfb5e830b
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-09-02 15:56:34 +0300

    Tooded ja Inimesed

--- MainClass.cs
+++ MainClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +14,98 @@ namespace TARpv23_CSharp
         public static void Main(string[] args)
         {
             Random random = new Random();
-            //I.osa Andmetüübid, Alamfunktsioonid,If
+            
             Console.OutputEncoding = Encoding.UTF8;
+            string text = "AAAAA BBB RRR OOOOO";
+            string[] texts = text.Split(' ');
+
+
+            //III. osa OOP
+
+            List<Inimene> inimesed = new List<Inimene>();
+            Inimene inimene1 = new Inimene();
+            inimene1.Nimi = "Pjotr 1";
+            inimene1.Vanus = 52;
+            inimene1.Sugu=Sugu.mees;
+            inimene1.Pikkus = 200;
+            inimene1.Kaal = 100;
+            Inimene inimene2 = new Inimene("Jelizaveta");
+            inimene2.Vanus = 98;
+            inimene2.Sugu = Sugu.naine;
+            inimene2.Pikkus = 160;
+            inimene2.Kaal = 70;
+            Inimene inimene3 = new Inimene("Marina", 18,Sugu.naine);
+            inimesed.Add(inimene1);
+            inimesed.Add(inimene2);
+            inimesed.Add(inimene3);
+            inimesed.Add(new Inimene("Irina",18,Sugu.naine));
+            foreach (Inimene inimene in inimesed)
+            {
+                Console.WriteLine(inimene.Nimi + " on " + inimene.Vanus + " aasta vana"+".Ta on "+inimene.Sugu+" SBI="+ inimene.HB_vorrand());
+                
+            }
+
+            Auto auto1=new Auto("681MRP",Color.Bisque,inimene1);
+            Auto auto2 = new Auto("180ABC", Color.NavajoWhite, inimene2);
+            Auto auto3 = new Auto("079TDI", Color.Red, inimene3);
+            auto1.KelleOmaAuto();
+            Dictionary<Auto,Inimene> register = new Dictionary<Auto,Inimene>();
+            register.Add(auto1, inimene1);
+            register.Add(auto2, inimene3);
+            register.Add(auto3, inimene3);
+            foreach (Auto item in register.Keys)
+            {
+                Console.WriteLine($"{item.Regnumber} - {item.Omanik.Nimi}");
+            }
+            foreach (KeyValuePair<Auto,Inimene> pair in register)
+            {
+                Console.WriteLine(pair.Key.Regnumber+"-"+pair.Value.Nimi);
+            }
+
+            //II. osa listid ja sõnastikud
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("Esimene");
+            arrayList.Add("Teine");
+            arrayList.Add("Kolmas");
+            Console.WriteLine("Otsing: ");
+            string vas=Console.ReadLine();
+            if (vas !=null && arrayList.Contains(vas))
+            {
+                Console.WriteLine("Otsitav element asub "+arrayList.IndexOf(vas)+". kohal");
+            }
+            else
+            {
+                Console.WriteLine("Kokku oli " + arrayList.Count + " elemente, vaid otsitav puudub");
+            }
+            arrayList.Clear();
+            arrayList.Insert(1, "Anna");
+            arrayList.Insert(0, "Inna");
+            
+
+
+
+
+
+            List<string> abc = new List<string>();
+            try
+            {
+                foreach (string rida in File.ReadAllLines(@"..\..\..\ABC.txt"))
+                {
+                    abc.Add(rida);
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Fail ei saa leida!");
+            }
+            foreach (string e in abc)
+            {
+                Console.Write(e);
+            }
+            Console.ReadLine();
+
+            //I.osa Andmetüübid, Alamfunktsioonid,If
             int N=random.Next(-10,101);
             int M = random.Next(-10, 101);
             int[] arvud;
